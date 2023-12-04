b94f648129c51b6fe48cd1a5c5feb81de2f4d7b9
Andy0223 <andy.lee@bluextrade.com>
2023-12-04 21:09:01 +0800

    add new shareValues and remove popUp and unused UI

--- Assets/Scripts/ShareValues.cs
+++ Assets/Scripts/ShareValues.cs
@@ -10,4 +10,9 @@ public class ShareValues : MonoBehaviour
     public static int ancestor4_counts = 20;
     public static int ancestor5_counts = 20;
     public static int ancestor6_counts = 20;
+
+    public static int boxOpenCounts = 0;
+    public static int UGSceneEntryCounts = 0;
+
+    public static int UGSceneTempPosition;
 }
