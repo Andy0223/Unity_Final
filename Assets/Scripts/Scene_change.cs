007b61e93a5ad1dfae72cc9a202e000812f82a7f
xChann <xxyt.95@gmail.com>
2023-12-07 19:41:24 +0800

    add Ch1_win&two scenes

--- Assets/Scripts/Scene_change.cs
+++ Assets/Scripts/Scene_change.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Start : MonoBehaviour
+public class Scene_change : MonoBehaviour
 {
+    // Start:0
+    // Ch1:1
+    // Ground:2
+    // UnderGround:3
     public void StartGame(){
         SceneManager.LoadSceneAsync(1);
     }
 
     public void StartGround(){
         SceneManager.LoadSceneAsync(2);
+    }
+
+    public void StartUnderGround(){
+        SceneManager.LoadSceneAsync(3);
     }
 }
