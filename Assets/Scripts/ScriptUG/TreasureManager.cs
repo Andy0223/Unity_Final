43431a25a66547f4551297647080fa3c7f374cf1
Yun <sandyfuoo68@gmail.com>
2023-12-16 04:08:52 +0800

    scenceConnect

---
+++ Assets/Scripts/ScriptUG/TreasureManager.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreasureManager : MonoBehaviour
+{
+    [SerializeField] private GameManager gameManager;
+    [SerializeField] private GameObject button;
+    [SerializeField] private GameObject backGround;
+    public GameObject[] treasures;
+    public GameObject[] Storys;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void CloseTreasure(string treasureName)
+    {
+        if(treasureName=="treasure1"){
+            treasures[0].SetActive(false);
+            ShareValues.treasure_1=false;
+            //增加祖先範例
+            ShareValues.ancestor1_counts +=2;
+            //故事1
+            Storys[0].SetActive(true);
+            button.SetActive(true);
+            backGround.SetActive(true);
+            //遊戲勝利
+            gameManager.GameOver();
+        }
+        else if(treasureName=="treasure2"){
+            treasures[1].SetActive(false);
+            ShareValues.treasure_2=false;
+            //增加祖先範例
+            ShareValues.ancestor1_counts +=2;
+            //故事2
+            Storys[1].SetActive(true);
+            button.SetActive(true);
+            backGround.SetActive(true);
+            //遊戲勝利
+            gameManager.GameOver();
+        }
+        else if(treasureName=="treasure3"){
+            treasures[2].SetActive(false);
+            ShareValues.treasure_3=false;
+            //增加祖先範例
+            ShareValues.ancestor1_counts +=2;
+            //故事3
+            Storys[2].SetActive(true);
+            button.SetActive(true);
+            backGround.SetActive(true);
+            //遊戲勝利
+            gameManager.GameOver();
+        }
+    }
+}
