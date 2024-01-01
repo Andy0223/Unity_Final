5aff280d88c4236639ff625d58aaea6ed61df367
Andy0223 <andy.lee@bluextrade.com>
2024-01-01 16:07:14 +0800

    build WebGL && light up UG backgroud && add ancestor bonus pop up && UG sign direction

--- Assets/Scripts/ScriptG/GameManagerGround.cs
+++ Assets/Scripts/ScriptG/GameManagerGround.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Text = TMPro.TextMeshProUGUI;
+using TextPro = TMPro.TextMeshProUGUI;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManagerGround : MonoBehaviour
 {
@@ -11,17 +12,32 @@ public class GameManagerGround : MonoBehaviour
     public float initialMinutes = 2f; // 初始分鐘
     public float initialSeconds = 0f; // 初始秒數
     public int EnemyDestroyCounts = 0;
-    public Text LevelText;
-    public Text TotalEnemyText;
+    public TextPro LevelText;
+    public TextPro TotalEnemyText;
     public GameObject teachingBox;
     public GameObject background;
+    public Text ancestor1_addon;
+    public Text ancestor2_addon;
+    public Text ancestor3_addon;
+    public Text ancestor4_addon;
+    public Text ancestor5_addon;
+    public Text ancestor6_addon;
+    public bool isStop = false;
 
     private int spawnEnemyCounts = 0;
     private int currentGameLevel;
-    public bool isStop = false;
-    
+    private bool isTreasure1Found = false;
+    private bool isTreasure2Found = false;
+    private bool isTreasure3Found = false;
+
     void Start()
     {
+        ancestor1_addon.enabled = false;
+        ancestor2_addon.enabled = false;
+        ancestor3_addon.enabled = false;
+        ancestor4_addon.enabled = false;
+        ancestor5_addon.enabled = false;
+        ancestor6_addon.enabled = false;
         currentGameLevel = ShareValues.UGSceneEntryCounts;
         Debug.Log("currentGameLevel: " + currentGameLevel);
         ShareValues.EnemyCounts = 5 * (currentGameLevel + 1);
@@ -29,6 +45,9 @@ public class GameManagerGround : MonoBehaviour
         LevelText.text = "Level: " + currentGameLevel;
         TotalEnemyText.text = "Total Enemy: " + ShareValues.EnemyCounts;
         isStop = false;
+        CheckAndShowTreasure(ShareValues.treasure_1, isTreasure1Found, ancestor1_addon, ancestor2_addon, ancestor3_addon, ancestor4_addon);
+        CheckAndShowTreasure(ShareValues.treasure_2, isTreasure2Found, ancestor3_addon, ancestor4_addon, ancestor5_addon, ancestor6_addon);
+        CheckAndShowTreasure(ShareValues.treasure_3, isTreasure3Found, ancestor5_addon, ancestor6_addon, ancestor1_addon, ancestor2_addon);
         StartCoroutine(SpawnEnemies());
         if (currentGameLevel != 0)
         {
@@ -64,6 +83,33 @@ public class GameManagerGround : MonoBehaviour
             }
             ChangetoUnderGround();
         }
+    }
+
+    void CheckAndShowTreasure(bool treasureFound, bool isFoundFlag, Text text1, Text text2, Text text3, Text text4)
+    {
+        if (treasureFound && !isFoundFlag)
+        {
+            Debug.LogWarning("Treasure found");
+
+            text1.enabled = true;
+            text2.enabled = true;
+            text3.enabled = true;
+            text4.enabled = true;
+
+            StartCoroutine(DisableTextAfterDelay(text1, text2, text3, text4));
+            isFoundFlag = true; // 設置為已找到
+        }
+    }
+
+    IEnumerator DisableTextAfterDelay(Text text1, Text text2, Text text3, Text text4)
+    {
+        yield return new WaitForSeconds(10f);
+
+        // 在這裡等待三秒後設置 Text 為 false
+        text1.enabled = false;
+        text2.enabled = false;
+        text3.enabled = false;
+        text4.enabled = false;
     }
 
     IEnumerator SpawnEnemies()
